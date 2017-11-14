a6149512a822cad2ddf8aee4479c089d645e2223
Kalil Peixoto <kalilmvp@gmail.com>
2017-11-14 19:09:55 +0100

    New components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
